5f34939acf478c006b65b46ec9ec6f9879df1407
Amir Boussejra <amir@epigno.com>
2022-07-15 16:47:44 +0200

    [language_model] microservice for python language model

---
+++ language_model/app/testing_db_connection.py
@@ -0,0 +1,27 @@
+import mysql.connector
+from mysql.connector import Error
+
+try:
+    connection = mysql.connector.connect(
+        user="root",
+        password="lama",
+        host="localhost",
+        port="3306",
+        database="jvc_topic_scrapping_db",
+        auth_plugin="mysql_native_password",
+    )
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = connection.cursor()
+        cursor.execute("select database();")
+        record = cursor.fetchone()
+        print("You're connected to database: ", record)
+
+except Error as e:
+    print("Error while connecting to MySQL", e)
+finally:
+    if connection.is_connected():
+        cursor.close()
+        connection.close()
+        print("MySQL connection is closed")
